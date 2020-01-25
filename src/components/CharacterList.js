437d773bbf1c7220245ba557e3709aa0b23d9819
johnnoory256 <noory.bs2016@gmail.com>
2020-01-24 17:21:10 -0800

    CharacterList and axios.get is completed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+  function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get('https://rickandmortyapi.com/api/character/?page=${page}')
+    .then((response => {
+      const data = response.data.results;
+      const result = data.filter((char) => char.name.includes(query));
+      setChars(result);
+    }))
+    .catch(error => {
+      console.log('there was a server error', error);
+    })
+  }, [query, page]);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
-}
+    const handleQuery = (event) => {
+      setQuery(event.target.value);
+    }
+
+    return (
+      <div>
+        <Link to="/"><button>Go Back Home</button></Link>
+       
+        <h2>Character List</h2>
+        {
+          chars.map((char, index) => {
+            return <CharacterCard key={index} char={char} />
+          })
+        }
+        
+      </div>
+    );
+  }
+
+export default CharacterList;
